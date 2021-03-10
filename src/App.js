4d7f586b242285ced9cd28a1795af23f964b000a
katieohern <katieohern@gmail.com>
2021-03-10 18:08:42 -0500

    overall design + blank chatbox

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
+import { faTimes } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import Button from 'react-bootstrap/Button';
+import Row from 'react-bootstrap/Row';
 import './App.css';
+import logo from './icons/logo.png';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Row className="App-header">
+        <h1 className="App-title">GatorCom</h1>
+        <img className="App-logo" src={logo} alt="Text Bubble"/>
+      </Row>
+      <div className="App-subheader">
+        <h2 className="App-subheader-text">{true ? "Start A Conversation" : "Iniciar Una Conversación"}</h2>
+        {true ? 
+          <div dangerouslySetInnerHTML={{__html: '<h2>Supports fluid interactions between <b>Spanish</b> and <b>English</b> speakers</h2>'}}/>
+          : <div dangerouslySetInnerHTML={{__html: '<h2>Admite interacciones fluidas entre hablantes de <b>Español</b> e <b>Inglés</b></h2>'}}/>
+        }
+        <Button className={true ? "App-subheader-switchbutton-english" : "App-subheader-switchbutton-spanish"}>
+          {true ? <>
+          <div><b>Switch To Spanish User</b></div>
+          <div><b>Cambiar Al Usario Español</b></div></> :
+          <><div><b>Cambiar Al Usuario Inglés</b></div>
+          <div><b>Switch To English User</b></div></>}     
+        </Button>
+      </div>
+      <div className="App-chatbox">
+          <div className="App-chatbox-card">
+            <div className="test">
+              <button className="App-exitbutton">
+                  <div className="App-exitbutton-group">
+                  <FontAwesomeIcon icon={faTimes} />
+                  {true ? <div className="App-exitbutton-label"><b>End</b></div> : <div className="App-exitbutton-label"><b>Fin</b></div>}
+                  </div>
+              </button>
+            </div>
+          </div>
+      </div>
+
     </div>
   );
 }
